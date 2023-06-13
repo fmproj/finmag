fd181bcf2312c12f44e00fc44298138f1042f831
Filip Merta <filmer.projects@gmail.com>
2023-06-13 12:10:24 +0200

    getting data from knight site

---
+++ apiknight.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { load } from 'cheerio';
+
+
+function knightApiRequest(name) {
+    const data = 'edice_magic=libovolna&rarita=A&foil=A&jmenokarty='+name+'&triditpodle=ceny&submit=Vyhledej';
+    
+    axios.post('https://cernyrytir.cz/index.php3?akce=3', data)
+        .then(res => {
+            console.log(`Status: ${res.status}`);
+            const $ = load(res.data);
+            const trs = $('.kusovkytext').eq(1).find('tbody').children('tr');
+
+            trs.each((index, tr) => {
+                const i = index % 3;
+                const tds = $(tr).children('td');
+
+                if (i == 0) {
+                    console.log(`Name: ${tds.eq(1).text()}`);
+                } else if (i == 1) {
+                    console.log(`Set: ${tds.eq(0).text()}`);
+                } else if (i == 2) {
+                    console.log(`Ks/Cena: ${tds.eq(1).text()} / ${tds.eq(2).text()}`);
+                }
+            });
+        })
+        .catch(err => {
+            console.error(err)
+        });
+}
+
+knightApiRequest('god-eternal+kefnet');
