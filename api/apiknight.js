e6a98b2686a053e04e1246289d8712f2e0d8b83d
Filip Merta <filmer.projects@gmail.com>
2023-06-19 12:19:36 +0200

    Returning data from apiknight in json format

--- api/apiknight.js
+++ api/apiknight.js
@@ -1,32 +1,43 @@
 import axios from 'axios';
 import { load } from 'cheerio';
 
-
-function knightApiRequest(name) {
-    const data = 'edice_magic=libovolna&rarita=A&foil=A&jmenokarty='+name+'&triditpodle=ceny&submit=Vyhledej';
-    
-    axios.post('https://cernyrytir.cz/index.php3?akce=3', data)
-        .then(res => {
-            console.log(`Status: ${res.status}`);
-            const $ = load(res.data);
-            const trs = $('table.kusovkytext').eq(1).find('tbody').children('tr');
-
-            trs.each((index, tr) => {
-                const i = index % 3;
-                const tds = $(tr).children('td');
-
-                if (i == 0) {
-                    console.log(`Name: ${tds.eq(1).text()}`);
-                } else if (i == 1) {
-                    console.log(`Set: ${tds.eq(0).text()}`);
-                } else if (i == 2) {
-                    console.log(`Ks/Cena: ${tds.eq(1).text()} / ${tds.eq(2).text()}`);
-                }
-            });
-        })
-        .catch(err => {
-            console.error(err)
+async function knightApiRequest(name) {
+    try {
+        const data = `edice_magic=libovolna&rarita=A&foil=A&jmenokarty=${encodeURIComponent(name)}&triditpodle=ceny&submit=Vyhledej`
+     
+        const response = await axios.post('https://cernyrytir.cz/index.php3?akce=3', data);
+        const $ = load(response.data);
+        const trs = $('table.kusovkytext').eq(1).find('tbody').children('tr');
+        const variants = [];
+        let variant;
+        
+        trs.each((index, tr) => {
+            const i = index % 3;
+            const tds = $(tr).children('td');
+            
+            if (i === 0) {
+                variant = {};
+                variant.name = tds.eq(1).text();
+            } else if (i === 1) {
+                variant.set = tds.eq(0).text();
+            } else if (i === 2) {
+                variant.quantity = tds.eq(1).text().split(/\u00A0/)[0];
+                variant.price = tds.eq(2).text().split(/\u00A0/)[0];    
+                variants.push(variant);
+            }
         });
+
+        const output = { variants };
+        return output;
+    } catch (err) {
+        throw err;
+    }
 }
 
-knightApiRequest('sol+ring');
+knightApiRequest('opposition agent')
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
